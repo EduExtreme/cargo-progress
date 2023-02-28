19ddf88288e695179acc592bb53b942b3de0c523
Eduardo Andre da Silva <edugexp@gmail.com>
2023-02-28 00:02:39 -0300

    feat: creating styles from app component

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Box, Flex } from "@chakra-ui/react";
+import { Spinner } from '@chakra-ui/react'
+
+
+export default function App() {
+
+  
+  return (
+    
+    <Box bg="blue.500">
+      OLA MUNDO, HELLO WORLD
+      <Spinner />
+      <Spinner color='red.500' />
+      </Box>
+  )
+}
+
+

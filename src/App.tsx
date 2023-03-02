e2dbf73cb8b30105b2c264640e79f3c1e8f84593
Eduardo André <edugexp@gmail.com>
2023-03-02 11:43:25 -0400

    wip: creating the first component and add prettier plugin at rules from vsCode

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,71 @@
-import { Box, Flex } from "@chakra-ui/react";
+import {
+  Box,
+  CircularProgress,
+  Flex,
+  Progress,
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+  Text,
+} from '@chakra-ui/react'
 import { Spinner } from '@chakra-ui/react'
+import { Fragment } from 'react'
 
+const Names = [
+  {
+    id: 1,
+    name: 'Navio A',
+    FirstLocation: 'South Africa',
+    currentLocation: 'Tokio',
+    lastLocation: 'Brazil ',
+    progress: 85,
+  },
+  {
+    id: 2,
+    name: 'Navio E',
+    progress: 12,
+  },
+  {
+    id: 3,
+    name: 'Navio C',
+    progress: 75,
+  },
+  {
+    id: 4,
+    name: 'Navio D',
+    progress: 57,
+  },
+  {
+    id: 5,
+    name: 'Navio B',
+    progress: 38,
+  },
+]
 
 export default function App() {
-
-  
   return (
-    
-    <Box bg="blue.500">
-      OLA MUNDO, HELLO WORLD
-      <Spinner />
-      <Spinner color='red.500' />
+    <>
+      <Box
+        my={{ base: '6', lg: '5rem' }}
+        px={{ base: '4', md: '8', lg: '10rem' }}
+        h="100%"
+      >
+        {Names.map((item) => (
+          <Box bg="black" mt="2rem" h="150px" key={item.id}>
+            {item.name}
+            {item.name}
+            <Flex flexDirection="column">
+              <Slider aria-label="slider-ex-1" defaultValue={item.progress}>
+                <SliderTrack>
+                  <SliderFilledTrack />
+                </SliderTrack>
+                <SliderThumb />
+              </Slider>
+            </Flex>
+          </Box>
+        ))}
       </Box>
+    </>
   )
 }
-
-
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider} from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import customTheme from './styles/theme'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-     <ChakraProvider>
+     <ChakraProvider theme={customTheme}>
         <App />
      </ChakraProvider>
     
